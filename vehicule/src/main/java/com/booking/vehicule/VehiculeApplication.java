ec642511b7ab5bc72878519a868bccb94f27df81
sayah-reda <reda1998.rs@gmail.com>
2025-10-07 13:00:08 +0100

    feat: add postgress config & vehicule

---
+++ vehicule/src/main/java/com/booking/vehicule/VehiculeApplication.java
@@ -0,0 +1,13 @@
+package com.booking.vehicule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculeApplication.class, args);
+	}
+
+}
